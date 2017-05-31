658a3d86da3ae36795b807102096ff35a99584bb
elonyong <elonyong@163.com>
2017-06-01 00:08:13 +0800

    添加导航

--- src/main/resources/static/js/modules/index.js
+++ src/main/resources/static/js/modules/index.js
@@ -2,9 +2,9 @@
  项目JS主入口
  以依赖Layui的layer和form模块为例
  **/
-layui.define(['layer', 'form'], function (exports) {
+layui.define(['layer', 'form','element'], function (exports) {
     var layer = layui.layer
-        , form = layui.form();
-    layer.msg('Hello World');
+        , form = layui.form()
+        , element = layui.element();
     exports('index', {}); //注意，这里是模块输出的核心，模块名必须和use时的模块名一致
 });
