c3cd42e96bb2e95c6032bf49db51c204a83b9192
elonyong <elonyong@163.com>
2017-06-02 15:07:48 +0800

    继续完善首页

--- src/main/java/com/elwyn/MainApplication.java
+++ src/main/java/com/elwyn/MainApplication.java
@@ -2,11 +2,18 @@ package com.elwyn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@Controller
 public class MainApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
+	}
+	@RequestMapping("/index")
+	public String index(){
+		return "html/index";
 	}
 }
